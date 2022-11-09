c14f93a0c82c8ca440f8c7fe4b3f29d3bc8f3366
Giannis Koumoutsos <koumoutsosgian@yahoo.gr>
2022-11-09 17:59:31 +0100

    added cypress and example tests

---
+++ cypress/e2e/firstpage.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('displays a welcome message', () => {
+    cy.get('h1').should('include.text', 'Welcome to the Ewoks-UI');
+    cy.location('pathname').should('not.include', 'edit-workflows');
+    cy.location('pathname').should('not.include', 'monitor-workflows');
+  });
+
+  it('offers 3 options to go ahead', () => {
+    cy.get('a').should('have.length', 3);
+  });
+
+  it('has an img of the canvas', () => {
+    cy.get('img').should('have.length', 1);
+  });
+
+  it('gets to editing when the appropriete button is pressed', () => {
+    cy.get('a').first().should('have.text', 'Edit Workflows');
+    cy.get('a').first().click();
+    cy.contains('tutorial_Graph').should('not.exist');
+    console.log(cy.location('pathname'));
+    cy.location('pathname').should('include', 'edit-workflows');
+    cy.go('back');
+  });
+
+  it('gets to editing when the appropriete button is pressed', () => {
+    cy.get('a').last().should('have.text', 'Tutorial Workflow');
+    cy.get('a').last().click();
+    cy.get('h1').should('include.text', 'tutorial_Graph');
+    // cy.get('button').should('have.length', 12);
+    // cy.location('pathname').should('include', '/#/edit-workflows');
+  });
+});
