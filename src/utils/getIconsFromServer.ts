8896acf24bddc3677e0753dc605f7e0536a0d463
Loic Huder <loic.huder@esrf.fr>
2022-12-07 14:15:36 +0100

    Fix eslint warnings in getIconsFromServer

--- src/utils/getIconsFromServer.ts
+++ src/utils/getIconsFromServer.ts
@@ -1,28 +1,27 @@
-/* eslint-disable promise/prefer-await-to-then */
-/* eslint-disable promise/prefer-await-to-callbacks */
 import { getIcon, getIcons } from './api';
 import path from 'path';
 import type { Icon } from '../types';
 
-async function getIconsFromServer(): Promise<Icon[] | object> {
+async function getIconsFromServer(): Promise<Icon[]> {
   const data = await getIcons();
 
-  return Promise.all(data.identifiers.map((id: string) => getIcon(id)))
-    .then((res) => {
-      const resCln = res.filter((result) => result.data !== null);
-      return resCln.map((result) => {
-        const imgbase64: { data_url: string } = (result.data as unknown) as {
-          data_url: string;
-        };
-        return {
-          name: path.basename(result.config.url),
-          image: imgbase64,
-          type: path.extname(result.config.url),
-        } as Icon;
-      });
-    })
-    .catch((error) => {
-      throw error;
+  const icons = await Promise.all(
+    data.identifiers.map((id: string) => getIcon(id))
+  );
+
+  return icons
+    .filter((result) => result.data !== null)
+    .map<Icon>((result) => {
+      const imgbase64 = (result.data as unknown) as {
+        data_url: string;
+      };
+
+      return {
+        name: path.basename(result.config.url),
+        image: imgbase64,
+        type: path.extname(result.config.url),
+      };
     });
 }
+
 export default getIconsFromServer;
