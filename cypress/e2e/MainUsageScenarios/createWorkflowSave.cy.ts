75fcc5a1443e278fa9b8b1bc6a9b01d3761199e8
Loic Huder <loic.huder@esrf.fr>
2022-12-08 11:50:22 +0100

    Use unique id for createWorflowSave test

--- cypress/e2e/MainUsageScenarios/createWorkflowSave.cy.ts
+++ cypress/e2e/MainUsageScenarios/createWorkflowSave.cy.ts
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid';
+
 describe('create workflow and save', () => {
   before(() => {
     cy.loadAppWithoutGraph();
@@ -10,47 +12,36 @@ describe('create workflow and save', () => {
   });
 
   it('gives a new unique name creates and deletes workflow', () => {
-    cy.window()
-      .its('__useStore__')
-      .then((store) => {
-        const { allWorkflows } = store.getState();
+    const id = nanoid();
 
-        let id = 11;
-        console.log(allWorkflows);
-        while (allWorkflows.some((work) => work.id === id.toString())) {
-          console.log(id);
-          id++;
-        }
+    cy.contains('New Name - Identifier')
+      .siblings('div')
+      .children('input')
+      .type(id.toString());
 
-        cy.contains('New Name - Identifier')
-          .siblings('div')
-          .children('input')
-          .type(id.toString(), { force: true });
+    cy.contains('Save Workflow').click();
+    cy.waitForStableDOM();
 
-        cy.contains('Save Workflow').click();
-        cy.waitForStableDOM();
+    cy.get('.react-flow__edge').should('have.length', 0);
+    cy.get('.react-flow__node').should('have.length', 0);
 
-        cy.get('.react-flow__edge').should('have.length', 0);
-        cy.get('.react-flow__node').should('have.length', 0);
+    cy.get('[data-testid="async-autocomplete-drop"]')
+      .click()
+      .get('input[type=text]')
+      .type(id.toString());
 
-        cy.get('[data-testid="async-autocomplete-drop"]')
-          .click()
-          .get('input[type=text]')
-          .type(id.toString());
+    cy.contains(id.toString()).click();
 
-        cy.contains(id.toString()).click();
+    cy.get(`[data-cy="${id.toString()}"]`).contains(id.toString());
 
-        cy.get(`[data-cy="${id.toString()}"]`).contains(id.toString());
+    cy.get(`[data-cy="tutorial_Graph"]`).should('not.exist');
 
-        cy.get(`[data-cy="tutorial_Graph"]`).should('not.exist');
+    cy.get(`[data-cy="sidebarDelete"]`).contains('Delete').click();
 
-        cy.get(`[data-cy="sidebarDelete"]`).contains('Delete').click();
+    cy.contains(`Delete "${id.toString()}" workflow?`);
 
-        cy.contains(`Delete "${id.toString()}" workflow?`);
+    cy.get(`[data-cy="yesButtonConfirmDialog"]`).contains('Yes').click();
 
-        cy.get(`[data-cy="yesButtonConfirmDialog"]`).contains('Yes').click();
-
-        cy.get(`[data-cy="${id.toString()}"]`).should('not.exist');
-      });
+    cy.get(`[data-cy="${id.toString()}"]`).should('not.exist');
   });
 });
