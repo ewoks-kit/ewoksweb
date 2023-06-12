eda82acd06d9df97c2b00fcfb5e88c1a54298d3a
giannis.koumoutsos <giannis.koumoutsos@esrf.fr>
2023-06-12 22:59:50 +0200

    no nodeDetails for input-output nodes

--- src/Components/Sidebar/details/ElementDetails.tsx
+++ src/Components/Sidebar/details/ElementDetails.tsx
@@ -1,18 +1,24 @@
 import LinkDetails from './LinkDetails';
 import NodeDetails from './NodeDetails';
 import GraphDetails from './GraphDetails';
-import { isNodeRF } from '../../../utils/typeGuards';
+import { isEdgeRF, isNodeRF } from '../../../utils/typeGuards';
 import type { SelectedElementRF } from '../../../types';
 
 function ElementDetails({ selectedElement }: SelectedElementRF) {
+  console.log(selectedElement);
+
   return (
     <form noValidate autoComplete="off" style={{ width: '100%' }}>
       {!selectedElement ? (
         <GraphDetails />
-      ) : isNodeRF(selectedElement) ? (
+      ) : isNodeRF(selectedElement) &&
+        selectedElement.type !== 'graphInput' &&
+        selectedElement.type !== 'graphOutput' ? (
         <NodeDetails key={selectedElement.id} {...selectedElement} />
       ) : (
-        <LinkDetails key={selectedElement.id} {...selectedElement} />
+        isEdgeRF(selectedElement) && (
+          <LinkDetails key={selectedElement.id} {...selectedElement} />
+        )
       )}
     </form>
   );
