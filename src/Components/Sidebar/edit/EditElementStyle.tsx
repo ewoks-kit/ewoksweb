8e58752837ea59c7b720b79892edd05dbe01397e
Giannis Koumoutsos <koumoutsosgian@yahoo.gr>
2023-07-03 16:14:51 +0200

    removed title for graph

--- src/Components/Sidebar/edit/EditElementStyle.tsx
+++ src/Components/Sidebar/edit/EditElementStyle.tsx
@@ -1,15 +1,10 @@
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import EditNodeStyle from './EditNodeStyle';
 import EditLinkStyle from './EditLinkStyle';
 import EditGraphStyle from './EditGraphStyle';
 import type { Node, Edge } from 'reactflow';
 import { isNodeRF } from '../../../utils/typeGuards';
 import type { SelectedElementRF } from '../../../types';
-import styles from '../EditSidebar.module.css';
 
 interface Content {
   title: string;
@@ -21,7 +16,7 @@ function getAccordionContent(
 ): Content {
   if (!selectedElement) {
     return {
-      title: 'Styling Graph',
+      title: '',
       EditComponent: EditGraphStyle,
     };
   }
@@ -50,8 +45,6 @@ export default function EditElementStyle({
       <Typography style={{ display: 'flex', justifyContent: 'center' }}>
         <span style={{ padding: '5px 0px 0px' }}>{title}</span>
       </Typography>
-      {/* <Typography>{title}</Typography> */}
-
       <form noValidate autoComplete="off">
         <EditComponent />
       </form>
