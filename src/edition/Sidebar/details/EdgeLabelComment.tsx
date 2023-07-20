481ad47e810df1caf921f3e1eea188b6fbc72429
Loic Huder <loic.huder@esrf.fr>
2023-07-20 14:43:54 +0200

    Remove unused CSS

--- src/edition/Sidebar/details/EdgeLabelComment.tsx
+++ src/edition/Sidebar/details/EdgeLabelComment.tsx
@@ -1,8 +1,6 @@
 import { FormControl, TextField } from '@material-ui/core';
-import SidebarTooltip from '../SidebarTooltip';
 import { Autocomplete } from '@material-ui/lab';
 import InputTextField from './InputTextField';
-import sidebarStyle from '../sidebarStyle';
 import { assertEdgeDataDefined, isDefined } from '../../../utils/typeGuards';
 import type { Edge } from 'reactflow';
 import { useReactFlow } from 'reactflow';
@@ -36,51 +34,39 @@ export default function EdgeLabelComment(props: Props) {
 
   return (
     <div className={styles.entry}>
-      {Object.keys(element).includes('source') && (
-        <SidebarTooltip text="Use Conditions or Data Mapping as label.">
-          <FormControl
-            fullWidth
-            variant="outlined"
-            style={{ ...sidebarStyle.formstyleflex }}
-          >
-            <Autocomplete
-              freeSolo
-              options={[
-                mappingToLabel(edgeData.data_mapping),
-                conditionsToLabel(edgeData.conditions),
-              ].filter(isDefined)}
-              value={element.label}
-              onChange={(e, value) => {
-                if (typeof value === 'string') {
-                  saveLabel(value);
-                }
-              }}
-              onInputChange={(e, value) => saveLabel(value)}
-              style={{ width: '98%' }}
-              renderInput={(params) => (
-                <TextField
-                  variant="outlined"
-                  margin="dense"
-                  style={{ margin: '0 0 8px 0', paddingTop: '2px' }}
-                  {...params}
-                  label="Label"
-                  multiline
-                />
-              )}
-            />
-          </FormControl>
-        </SidebarTooltip>
-      )}
-
-      <div style={{ display: 'block' }}>
-        <InputTextField
-          label="Comment"
-          defaultValue={edgeData.comment}
-          onValueSave={(newComment) => {
-            mergeEdgeData(element.id, { comment: newComment });
+      <FormControl fullWidth variant="outlined">
+        <Autocomplete
+          freeSolo
+          options={[
+            mappingToLabel(edgeData.data_mapping),
+            conditionsToLabel(edgeData.conditions),
+          ].filter(isDefined)}
+          value={element.label}
+          onChange={(e, value) => {
+            if (typeof value === 'string') {
+              saveLabel(value);
+            }
           }}
+          onInputChange={(e, value) => saveLabel(value)}
+          renderInput={(params) => (
+            <TextField
+              variant="outlined"
+              margin="dense"
+              {...params}
+              label="Label"
+              multiline
+            />
+          )}
         />
-      </div>
+      </FormControl>
+
+      <InputTextField
+        label="Comment"
+        defaultValue={edgeData.comment}
+        onValueSave={(newComment) => {
+          mergeEdgeData(element.id, { comment: newComment });
+        }}
+      />
     </div>
   );
 }
