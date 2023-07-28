18adb9a5bf375b717726366bb9920dd2639e5404
Loic Huder <loic.huder@esrf.fr>
2023-07-28 15:10:20 +0200

    Add tests for monitor page

---
+++ cypress/e2e/monitor.cy.ts
@@ -0,0 +1,52 @@
+describe('Monitor workflows', () => {
+  before(() => {
+    cy.visit('http://localhost:3000/monitor');
+  });
+
+  it('should display no execution event in the history', () => {
+    cy.findByText('No workflow was run!').should('exist');
+    cy.findByRole('list').should('not.exist');
+  });
+
+  it('should redirect to monitor page and display "Success" when successfully executing a workflow', () => {
+    cy.visit('http://localhost:3000/edit');
+    cy.loadGraph('demo');
+
+    cy.findByRole('button', { name: 'Open menu with more actions' }).click();
+    cy.findByRole('menuitem', { name: 'Execute workflow' }).click();
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/monitor');
+    });
+
+    // Wait until socket messages are sent
+    cy.wait(1000);
+
+    cy.findAllByRole('listitem').should('have.length', 1);
+
+    cy.findByRole('listitem', { name: 'demo' }).within(() => {
+      cy.findByText('Success').should('exist');
+    });
+  });
+
+  it('should be able to display the status of another workflow (even if it cannot be executed)', () => {
+    cy.visit('http://localhost:3000/edit');
+    cy.loadGraph('Ewoks-Tasks');
+
+    cy.findByRole('button', { name: 'Open menu with more actions' }).click();
+    cy.findByRole('menuitem', { name: 'Execute workflow' }).click();
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/monitor');
+    });
+
+    // Wait until socket messages are sent
+    cy.wait(1000);
+
+    cy.findAllByRole('listitem').should('have.length', 2);
+
+    cy.findByRole('listitem', { name: 'Ewoks-Tasks' }).within(() => {
+      cy.findByText('Failed').should('exist');
+    });
+  });
+});
