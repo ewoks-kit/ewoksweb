4ed88c8db0aff7c92d1df2f206cb95bcaecd1ea0
Giannis Koumoutsos <koumoutsosgian@yahoo.gr>
2023-08-25 18:06:56 +0200

    initial changes

--- src/edition/Canvas/Canvas.tsx
+++ src/edition/Canvas/Canvas.tsx
@@ -206,13 +206,8 @@ function Canvas() {
       },
       ewoks_props: {
         label: trimLabel(task_identifier),
-        default_inputs: [],
         inputs_complete: false,
         default_error_node: false,
-        default_error_attributes: {
-          map_all_data: true,
-          data_mapping: [],
-        },
       },
       ui_props: {
         icon,
