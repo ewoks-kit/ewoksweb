77766abcafa7d80fa9d58ebecb5f39cb51f6171d
Loic Huder <loic.huder@esrf.fr>
2023-10-09 16:27:38 +0200

    Remove unneeded ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,14 @@
 import 'normalize.css';
 import './styles/index.css';
 
-import {
-  createTheme,
-  StyledEngineProvider,
-  ThemeProvider,
-} from '@mui/material';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const theme = createTheme();
-
 ReactDOM.render(
   <StrictMode>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <App />
   </StrictMode>,
   document.querySelector('#root'),
 );
