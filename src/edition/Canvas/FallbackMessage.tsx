18644758d71ce0a32858aa6a2147dc08f19f7da4
Loic Huder <loic.huder@esrf.fr>
2023-12-11 17:22:44 +0100

    Focus and open Quick Open dropdown when clicking on the fallback message

--- src/edition/Canvas/FallbackMessage.tsx
+++ src/edition/Canvas/FallbackMessage.tsx
@@ -1,21 +1,31 @@
+import useQuickOpenDropdownState from '../../general/useQuickOpenDropdownState';
 import { useNodesLength } from '../../store/graph-hooks';
 import styles from './FallbackMessage.module.css';
 
 export default function FallbackMessage() {
   const nodesCount = useNodesLength();
+  const quickOpenDropdown = useQuickOpenDropdownState((state) => state.element);
 
-  if (nodesCount !== 0) {
+  if (!quickOpenDropdown || nodesCount !== 0) {
     return null;
   }
 
   return (
-    <div className={styles.noWorkflowMessage}>
+    <div className={styles.container}>
       <p>
-        <strong>Drag and drop</strong> tasks here to start building your
-        workflow,
+        Drag and drop tasks here to start building your workflow,
         <br />
-        or use <em>Quick Open</em> to <strong>open</strong> an existing
-        workflow.
+        or use{' '}
+        <button
+          className={styles.btn}
+          onClick={() => {
+            quickOpenDropdown.click();
+          }}
+          type="button"
+        >
+          Quick Open
+        </button>{' '}
+        to open an existing workflow.
       </p>
     </div>
   );
