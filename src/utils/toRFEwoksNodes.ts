71dfc9385078f6007d32386162a11a4323f7547e
Loic Huder <loic.huder@esrf.fr>
2024-01-24 11:14:10 +0100

    Clarify some variables and function names

--- src/utils/toRFEwoksNodes.ts
+++ src/utils/toRFEwoksNodes.ts
@@ -11,33 +11,26 @@ import { outNodesLinks } from './outNodesLinks';
 import { calcSubgraphIO, calcTaskProps } from './toRFEwoksNodesUtils';
 import { createDataMappingData, notUndefinedValue } from './utils';
 
-// Accepts a GraphEwoks and returns an EwoksRFNode[]
-export function toRFEwoksNodes(
-  tempGraph: Workflow,
-  newNodeSubgraphs: Workflow[],
+export function convertEwoksWorkflowToRFNodes(
+  workflowToConvert: Workflow,
+  subWorkflows: Workflow[],
   tasks: Task[],
 ): NodeWithData[] {
-  const inNodeLinks = inNodesLinks(
-    tempGraph.graph.input_nodes,
-    tempGraph.nodes,
+  const { nodes } = workflowToConvert;
+
+  const { nodes: inputNodes } = inNodesLinks(
+    workflowToConvert.graph.input_nodes,
+    nodes,
   );
-  const outNodeLinks = outNodesLinks(
-    tempGraph.graph.output_nodes,
-    tempGraph.nodes,
+  const { nodes: outputNodes } = outNodesLinks(
+    workflowToConvert.graph.output_nodes,
+    nodes,
   );
 
-  const inOutTempGraph = { ...tempGraph };
+  const allNodes = [...nodes, ...inputNodes, ...outputNodes];
 
-  if (inNodeLinks.nodes.length > 0) {
-    inOutTempGraph.nodes = [...inOutTempGraph.nodes, ...inNodeLinks.nodes];
-  }
-
-  if (outNodeLinks.nodes.length > 0) {
-    inOutTempGraph.nodes = [...inOutTempGraph.nodes, ...outNodeLinks.nodes];
-  }
-
-  return inOutTempGraph.nodes.map(
-    ({
+  return allNodes.map((node) => {
+    const {
       id,
       task_type,
       task_identifier,
@@ -48,55 +41,52 @@ export function toRFEwoksNodes(
       default_error_attributes,
       task_generator,
       uiProps,
-    }) => {
-      const node: NodeWithData = {
-        id: id.toString(),
-        type: task_type,
-        data: {
-          ewoks_props: {
-            label,
-            ...(default_inputs &&
-              default_inputs.length > 0 && {
-                default_inputs: default_inputs.map((dIn) => {
-                  return {
-                    name: dIn.name.toString(),
-                    value: dIn.value,
-                  };
-                }),
+    } = node;
+    return {
+      id: id.toString(),
+      type: task_type,
+      data: {
+        ewoks_props: {
+          label,
+          ...(default_inputs &&
+            default_inputs.length > 0 && {
+              default_inputs: default_inputs.map((dIn) => {
+                return {
+                  name: dIn.name.toString(),
+                  value: dIn.value,
+                };
               }),
-            ...notUndefinedValue(inputs_complete, 'inputs_complete'),
-            ...notUndefinedValue(default_error_node, 'default_error_node'),
-            ...(default_error_node &&
-              default_error_attributes && {
-                default_error_attributes: calcDefaultErrorAttributes(
-                  default_error_attributes,
-                ),
-              }),
-            ...notUndefinedValue(task_generator, 'task_generator'),
-          },
-          task_props: calcTaskProps(task_identifier, tasks) || {
-            task_identifier,
-            task_type,
-          },
-          ui_props: {
-            ...notUndefinedValue(uiProps?.nodeWidth, 'nodeWidth'),
-            ...(uiProps?.icon && { icon: uiProps.icon }),
-            ...notUndefinedValue(uiProps?.moreHandles, 'moreHandles'),
-            ...notUndefinedValue(uiProps?.withImage, 'withImage'),
-            ...notUndefinedValue(uiProps?.withLabel, 'withLabel'),
-            ...notUndefinedValue(uiProps?.colorBorder, 'colorBorder'),
-            ...(task_type === 'graph'
-              ? calcSubgraphIO(newNodeSubgraphs, task_identifier)
-              : {}),
-          },
-          ...notUndefinedValue(uiProps?.comment, 'comment'),
+            }),
+          ...notUndefinedValue(inputs_complete, 'inputs_complete'),
+          ...notUndefinedValue(default_error_node, 'default_error_node'),
+          ...(default_error_node &&
+            default_error_attributes && {
+              default_error_attributes: calcDefaultErrorAttributes(
+                default_error_attributes,
+              ),
+            }),
+          ...notUndefinedValue(task_generator, 'task_generator'),
         },
-        position: uiProps?.position ?? { x: 100, y: 100 },
-      };
-
-      return node;
-    },
-  );
+        task_props: calcTaskProps(task_identifier, tasks) || {
+          task_identifier,
+          task_type,
+        },
+        ui_props: {
+          ...notUndefinedValue(uiProps?.nodeWidth, 'nodeWidth'),
+          ...(uiProps?.icon && { icon: uiProps.icon }),
+          ...notUndefinedValue(uiProps?.moreHandles, 'moreHandles'),
+          ...notUndefinedValue(uiProps?.withImage, 'withImage'),
+          ...notUndefinedValue(uiProps?.withLabel, 'withLabel'),
+          ...notUndefinedValue(uiProps?.colorBorder, 'colorBorder'),
+          ...(task_type === 'graph'
+            ? calcSubgraphIO(subWorkflows, task_identifier)
+            : {}),
+        },
+        ...notUndefinedValue(uiProps?.comment, 'comment'),
+      },
+      position: uiProps?.position ?? { x: 100, y: 100 },
+    };
+  });
 }
 
 function calcDefaultErrorAttributes(
