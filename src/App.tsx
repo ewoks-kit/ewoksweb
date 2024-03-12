4d91356d97a889bcdc43d4e173d5fa36d813812a
Giannis Koumoutsos <koumoutsosgian@yahoo.gr>
2024-03-12 15:35:50 +0100

    use the default pop-up without saving

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Navigate, Outlet, Routes } from 'react-router-dom';
-import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { apiSuffix, baseUrl } from './api/client';
 import EditRoute from './EditRoute';
@@ -26,6 +26,10 @@ const router = createBrowserRouter(
           path: 'monitor',
           element: <MonitorRoute />,
         },
+        {
+          path: '/',
+          element: <Navigate to="edit" replace />,
+        },
       ],
     },
   ],
@@ -40,18 +44,6 @@ function Layout() {
       <NavBar />
       <Outlet />
     </>
-  );
-}
-
-function Root() {
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        {/* <Route path="/edit" element={<EditRoute />} />
-        <Route path="/monitor" element={<MonitorRoute />} /> */}
-        {/* <Route path="/*" element={<Navigate to="/edit" replace />} /> */}
-      </Route>
-    </Routes>
   );
 }
 
