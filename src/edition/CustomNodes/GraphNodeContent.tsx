b66499c8af03d849d234251928d7fb630a4c0dfc
Loic Huder <loic.huder@esrf.fr>
2024-09-27 14:42:24 +0200

    Clean node styles

--- src/edition/CustomNodes/GraphNodeContent.tsx
+++ src/edition/CustomNodes/GraphNodeContent.tsx
@@ -1,4 +1,3 @@
-import { Tooltip } from '@mui/material';
 import { memo, useEffect, useMemo } from 'react';
 import type { NodeProps } from 'reactflow';
 import { Handle, Position } from 'reactflow';
@@ -10,9 +9,10 @@ import SuspenseBoundary from '../../suspense/SuspenseBoundary';
 import type { NodeData } from '../../types';
 import { DEFAULT_NODE_VALUES } from '../../utils/defaultValues';
 import { assertNodeDataDefined } from '../../utils/typeGuards';
+import NodeContent from './NodeContent';
 import NodeIcon from './NodeIcon';
 import NodeLabel from './NodeLabel';
-import { contentStyle, style } from './nodeStyles';
+import { contentStyle } from './nodeStyles';
 
 function GraphNodeContent(props: NodeProps<NodeData>) {
   const { id } = props;
@@ -45,148 +45,136 @@ function GraphNodeContent(props: NodeProps<NodeData>) {
     subgraphExistsOnServer,
   ]);
 
-  const { inputs = [], outputs = [] } = uiProps;
-
-  const nodeWidth = { width: `${uiProps.nodeWidth || 100}px` };
+  const { inputs = [], outputs = [], nodeWidth, colorBorder } = uiProps;
   const { withImage = DEFAULT_NODE_VALUES.uiProps.withImage } = uiProps;
   const { withLabel = DEFAULT_NODE_VALUES.uiProps.withLabel } = uiProps;
 
   return (
-    <div className="node-content" style={{ borderColor: uiProps.colorBorder }}>
-      <Tooltip
-        title={
-          nodeData.comment ? (
-            <span style={style.comment}>{nodeData.comment}</span>
-          ) : (
-            ''
-          )
-        }
-        enterDelay={800}
-        arrow
-      >
-        <span style={{ ...style.displayNode, ...nodeWidth }} className="icons">
-          <NodeLabel
-            id={id}
-            label={nodeData.ewoks_props.label}
-            showFull={withLabel}
-            showCropped={!withLabel && !withImage}
-            color={subgraphExistsOnServer ? '#ced3ee' : 'red'}
-          />
-          {withImage && (
-            <SuspenseBoundary>
-              <NodeIcon nodeId={id} />
-            </SuspenseBoundary>
-          )}
-          <span style={style.contentWrapper}>
-            {inputs.length === 0 ? (
+    <NodeContent
+      width={nodeWidth}
+      borderColor={colorBorder}
+      tooltip={nodeData.comment}
+    >
+      <NodeLabel
+        id={id}
+        label={nodeData.ewoks_props.label}
+        showFull={withLabel}
+        showCropped={!withLabel && !withImage}
+        color={subgraphExistsOnServer ? '#ced3ee' : 'red'}
+      />
+      {withImage && (
+        <SuspenseBoundary>
+          <NodeIcon nodeId={id} />
+        </SuspenseBoundary>
+      )}
+      <span style={{ padding: '8px 0px' }}>
+        {inputs.length === 0 ? (
+          <div
+            style={{
+              ...contentStyle.io,
+              ...contentStyle.textLeft,
+            }}
+          >
+            No input provided
+          </div>
+        ) : (
+          inputs
+            .sort((a, b) => (a.positionY || 0) - (b.positionY || 0))
+            .map((input: { label: string }) => (
               <div
+                key={input.label}
                 style={{
                   ...contentStyle.io,
                   ...contentStyle.textLeft,
+                  ...(uiProps.moreHandles ? contentStyle.borderInput : {}),
                 }}
               >
-                No input provided
-              </div>
-            ) : (
-              inputs
-                .sort((a, b) => (a.positionY || 0) - (b.positionY || 0))
-                .map((input: { label: string }) => (
-                  <div
-                    key={input.label}
+                {/* remove the rest of the input {input.label} for now */}
+                {input.label.slice(0, input.label.indexOf(':'))}
+                <Handle
+                  key={input.label}
+                  type="target"
+                  position={Position.Left}
+                  id={input.label.slice(0, input.label.indexOf(':'))}
+                  style={{
+                    ...contentStyle.handle,
+                    ...contentStyle.left,
+                    ...contentStyle.handleTarget,
+                  }}
+                />
+                {uiProps.moreHandles && (
+                  <Handle
+                    key={`${input.label} right`}
+                    type="target"
+                    position={Position.Right}
+                    id={`${input.label.slice(
+                      0,
+                      input.label.indexOf(':'),
+                    )} right`}
                     style={{
-                      ...contentStyle.io,
-                      ...contentStyle.textLeft,
-                      ...(uiProps.moreHandles ? contentStyle.borderInput : {}),
+                      ...contentStyle.handle,
+                      ...contentStyle.right,
+                      ...contentStyle.handleTarget,
                     }}
-                  >
-                    {/* remove the rest of the input {input.label} for now */}
-                    {input.label.slice(0, input.label.indexOf(':'))}
-                    <Handle
-                      key={input.label}
-                      type="target"
-                      position={Position.Left}
-                      id={input.label.slice(0, input.label.indexOf(':'))}
-                      style={{
-                        ...contentStyle.handle,
-                        ...contentStyle.left,
-                        ...contentStyle.handleTarget,
-                      }}
-                    />
-                    {uiProps.moreHandles && (
-                      <Handle
-                        key={`${input.label} right`}
-                        type="target"
-                        position={Position.Right}
-                        id={`${input.label.slice(
-                          0,
-                          input.label.indexOf(':'),
-                        )} right`}
-                        style={{
-                          ...contentStyle.handle,
-                          ...contentStyle.right,
-                          ...contentStyle.handleTarget,
-                        }}
-                      />
-                    )}
-                  </div>
-                ))
-            )}
-            {outputs.length === 0 ? (
+                  />
+                )}
+              </div>
+            ))
+        )}
+        {outputs.length === 0 ? (
+          <div
+            style={{
+              ...contentStyle.io,
+              ...contentStyle.textRight,
+            }}
+          >
+            No output provided
+          </div>
+        ) : (
+          outputs
+            .sort((a, b) => (a.positionY || 0) - (b.positionY || 0))
+            .map((output: { label: string }) => (
               <div
+                key={output.label}
                 style={{
                   ...contentStyle.io,
                   ...contentStyle.textRight,
+                  ...(uiProps.moreHandles ? contentStyle.borderOutput : {}),
                 }}
               >
-                No output provided
-              </div>
-            ) : (
-              outputs
-                .sort((a, b) => (a.positionY || 0) - (b.positionY || 0))
-                .map((output: { label: string }) => (
-                  <div
-                    key={output.label}
+                {output.label.slice(0, output.label.indexOf(':'))}
+                <Handle
+                  key={output.label}
+                  type="source"
+                  position={Position.Right}
+                  id={output.label.slice(0, output.label.indexOf(':'))}
+                  style={{
+                    ...contentStyle.handle,
+                    ...contentStyle.right,
+                    ...contentStyle.handleSource,
+                  }}
+                />
+                {uiProps.moreHandles && (
+                  <Handle
+                    key={`${output.label} left`}
+                    type="source"
+                    position={Position.Left}
+                    id={`${output.label.slice(
+                      0,
+                      output.label.indexOf(':'),
+                    )} left`}
                     style={{
-                      ...contentStyle.io,
-                      ...contentStyle.textRight,
-                      ...(uiProps.moreHandles ? contentStyle.borderOutput : {}),
+                      ...contentStyle.handle,
+                      ...contentStyle.left,
+                      ...contentStyle.handleSource,
                     }}
-                  >
-                    {output.label.slice(0, output.label.indexOf(':'))}
-                    <Handle
-                      key={output.label}
-                      type="source"
-                      position={Position.Right}
-                      id={output.label.slice(0, output.label.indexOf(':'))}
-                      style={{
-                        ...contentStyle.handle,
-                        ...contentStyle.right,
-                        ...contentStyle.handleSource,
-                      }}
-                    />
-                    {uiProps.moreHandles && (
-                      <Handle
-                        key={`${output.label} left`}
-                        type="source"
-                        position={Position.Left}
-                        id={`${output.label.slice(
-                          0,
-                          output.label.indexOf(':'),
-                        )} left`}
-                        style={{
-                          ...contentStyle.handle,
-                          ...contentStyle.left,
-                          ...contentStyle.handleSource,
-                        }}
-                      />
-                    )}
-                  </div>
-                ))
-            )}
-          </span>
-        </span>
-      </Tooltip>
-    </div>
+                  />
+                )}
+              </div>
+            ))
+        )}
+      </span>
+    </NodeContent>
   );
 }
 
--- src/edition/CustomNodes/NoteNode.tsx
+++ src/edition/CustomNodes/NoteNode.tsx
@@ -3,6 +3,7 @@ import type { NodeProps } from 'reactflow';
 import useNodeDataStore from '../../store/useNodeDataStore';
 import type { NodeData } from '../../types';
 import { assertNodeDataDefined } from '../../utils/typeGuards';
+import NodeContent from './NodeContent';
 import NodeLabel from './NodeLabel';
 
 type NoteProps = NodeProps<NodeData>;
@@ -11,29 +12,18 @@ function NoteNode(args: NoteProps) {
   const nodeData = useNodeDataStore((state) => state.nodesData.get(args.id));
   assertNodeDataDefined(nodeData, args.id);
 
-  const uiProps = nodeData.ui_props;
+  const { nodeWidth, colorBorder } = nodeData.ui_props;
 
   return (
-    <div
-      className="node-content"
-      style={{
-        padding: '10px',
-        borderColor: uiProps.colorBorder,
-      }}
-    >
-      <div
-        style={{ maxWidth: `${uiProps.nodeWidth || 100}px` }}
-        className="icons"
-      >
-        <NodeLabel
-          id={args.id}
-          label={nodeData.ewoks_props.label}
-          showFull
-          color="#ced3ee"
-        />
-        <div style={{ wordWrap: 'break-word' }}>{nodeData.comment}</div>
-      </div>
-    </div>
+    <NodeContent width={nodeWidth} borderColor={colorBorder}>
+      <NodeLabel
+        id={args.id}
+        label={nodeData.ewoks_props.label}
+        showFull
+        color="#ced3ee"
+      />
+      <div style={{ wordWrap: 'break-word' }}>{nodeData.comment}</div>
+    </NodeContent>
   );
 }
 
